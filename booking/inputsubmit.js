1596bc11e5042ed04632fd120618db34b9df09b8
Sunnyshineshow <sun.napon11@gmail.com>
2021-07-14 16:09:45 +0700

    Add new script - submit script

---
+++ booking/inputsubmit.js
@@ -0,0 +1,18 @@
+console.log("Script is working properly")
+
+$(document).ready(() => 
+{
+    console.log("inputsubmit is ready")
+    $("#submit_button").click(() =>
+    {
+        let data = 
+        {
+            name: $("#event_name").val(),
+            date_start: $("#datetime_start").val(),
+            datetime_end: $("#datetime_end").val(),
+            desc: $("#event_description").val()
+        }
+
+        document.getElementById("log").innerHTML = data.name + " " + data.date_start + " " + data.datetime_end + " " + data.desc
+    })
+})
